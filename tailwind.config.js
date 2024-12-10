1e4120472d20936699f26b7f82fc4a0350ca2570
AlbertoLyons <alberto.lyons@alumnos.ucn.cl>
2024-12-10 12:01:42 -0300

    feat: added flowbite and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"   
+  ],
+  plugins: [
+      require('flowbite/plugin')
+  ],
+  content: [
+    "./node_modules/flowbite/**/*.js"
+]
+  
+}
+
