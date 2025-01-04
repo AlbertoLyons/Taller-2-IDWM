713ff0e321702ef180bba17b45748990db402a96
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-01-04 12:06:58 -0300

    feat(view-products-admin): Added imports

--- src/app/e-commerce/pages/view-products-admin/view-products-admin.component.ts
+++ src/app/e-commerce/pages/view-products-admin/view-products-admin.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-view-products-admin',
-  imports: [],
+  imports: [
+    NavbarComponent,
+    SearchBarComponent],
   templateUrl: './view-products-admin.component.html',
   styleUrl: './view-products-admin.component.css'
 })
