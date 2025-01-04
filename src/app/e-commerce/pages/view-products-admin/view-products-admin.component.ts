b13056dfec8f4b3b6c62e9dd64bcee59d4803753
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-01-04 12:58:32 -0300

    feat(view-products-admin):Added methods and functions to display products

--- src/app/e-commerce/pages/view-products-admin/view-products-admin.component.ts
+++ src/app/e-commerce/pages/view-products-admin/view-products-admin.component.ts
@@ -1,15 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+import { ChangePageButtonsComponent } from "../../components/change-page-buttons/change-page-buttons.component";
+import { HttpClientModule } from '@angular/common/http';
+import { ProductServices } from '../../services/products.service';
+import { Product } from '../../interfaces/ResponseApi_products';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-products-admin',
   imports: [
+    CommonModule,
     NavbarComponent,
-    SearchBarComponent],
+    SearchBarComponent,
+    ChangePageButtonsComponent,
+    HttpClientModule
+],
+  providers: [ProductServices],
   templateUrl: './view-products-admin.component.html',
   styleUrl: './view-products-admin.component.css'
 })
 export class ViewProductsAdminComponent {
 
+  private productServices: ProductServices = inject(ProductServices);
+  products: Product[] = [];
+  actualPage: number = 1;
+  order: string = "asc";
+  type: string = "Nada";
+
+  pageChange = output<number>();
+  maxPage: number = 0;
+  buttonNextDisabled: boolean = true;
+  buttonPreviousDisabled: boolean = false;
+
+  constructor(private router: Router) {
+    this.getAllProductUsers(this.order, this.type, this.actualPage);
+    this.getMaxPage();
+  }
+
+  async getMaxPage(){
+    this.maxPage = await this.productServices.getMaxPage()
+    .catch((error) => {
+      console.log(error);
+      return 0;
+    }) || 0;
+  }
+
+  async getAllProductUsers(AscOrDesc:string, type : string, page: number) {
+    this.products = await this.productServices.getProductsUsers(AscOrDesc, type,  page)
+    .catch((error) => {
+      console.log(error);
+      return [];
+    }) || [];
+  }
+
+  setNewPage(page: number) {
+    if (page === 1) {
+      this.buttonPreviousDisabled = true;
+    } else {
+      this.buttonPreviousDisabled = false;
+    }
+    if (page === this.maxPage) {
+      this.buttonNextDisabled = true;
+    } else {
+      this.buttonNextDisabled = false;
+    }
+    this.pageChange.emit(page);
+    this.getAllProductUsers(this.order, this.type, page);
+  }
 }
