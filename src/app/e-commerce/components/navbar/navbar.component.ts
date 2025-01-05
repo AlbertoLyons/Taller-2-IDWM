f9ae81ac72083f824ad8ae91fd493e0f54cac1c9
NicolasGO2112 <nicolas.galleguillos01@alumnos.ucn.cl>
2025-01-04 21:23:01 -0300

    feat: added cart page and the loggin charge the view-products page or view-products-admin page based on the rol of the user

--- src/app/e-commerce/components/navbar/navbar.component.ts
+++ src/app/e-commerce/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
--- src/app/e-commerce/pages/cart/cart.component.ts
+++ src/app/e-commerce/pages/cart/cart.component.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { CartService } from '../../services/cart.service';
+import { ProductCart } from '../../interfaces/ResponseApi_cart';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CurrencyPipe, CommonModule, FormsModule],
+  providers: [CartService],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
 
+
+
+  private productServices: CartService = inject(CartService);
+  productsCart: ProductCart[] = [];
+  totalPrice: number = 0;
+  pais: string = '';
+  ciudad: string = '';
+  comuna: string = '';
+  calle: string = '';
+  constructor(private router: Router, private cdr: ChangeDetectorRef) {
+    this.getAllProductCart();
+    this.cdr.markForCheck();
+    console.log(this.productsCart);
+
+  }
+  async getAllProductCart(){
+    this.productsCart = await this.productServices.getProductFromCart()
+    .catch((error) => {
+      console.log(error);
+      return [];
+    }) || [];
+    console.log(this.productsCart);
+    this.calculateTotal();
+  }
+  async increaseQuantity(id: number) {
+    const response = await this.productServices.increaseOrDecreaseProduct(id, "add")
+    .catch((error) => {
+      console.log(error);
+      return [];
+    }) || [];
+    console.log(response);
+    this.getAllProductCart();
+    this.calculateTotal();
+  }
+  async decreaseQuantity(id: number) {
+    const response = await this.productServices.increaseOrDecreaseProduct(id, "decrease")
+    .catch((error) => {
+      console.log(error);
+      return [];
+    }) || [];
+    console.log(response);
+    this.getAllProductCart();
+    this.calculateTotal();
+  }
+  finalizePurchase() {
+    if (this.isFormValid()) {
+      console.log('Compra finalizada');
+      // Aquí podrías hacer la llamada al servicio para procesar la compra
+    }
+  }
+  async removeProduct(idProducto: number) {
+    const response = await this.productServices.deleteProductFromCart(idProducto)
+    .catch((error) => {
+      console.log(error);
+      return [];
+    }) || [];
+    console.log(response);
+    this.getAllProductCart();
+    this.calculateTotal();
+  }
+  calculateTotal(): void {
+    this.totalPrice = this.productsCart.reduce((total, product) => {
+      return total + (product.price * product.quantity);  // Multiplica precio por cantidad y acumula
+    }, 0);
+  }
+  isFormValid(): boolean {
+    return this.pais.trim() !== '' && this.ciudad.trim() !== '' && this.comuna.trim() !== '' && this.calle.trim() !== '';
+  }
 }
