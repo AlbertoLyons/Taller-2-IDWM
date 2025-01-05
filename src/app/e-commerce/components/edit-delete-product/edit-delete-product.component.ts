2a71044317275e65804dcfe6acf0e7c67e5ddfd1
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-01-05 12:02:21 -0300

    feat(edit-delete-admin): Added component

---
+++ src/app/e-commerce/components/edit-delete-product/edit-delete-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-delete-product',
+  imports: [],
+  templateUrl: './edit-delete-product.component.html',
+  styleUrl: './edit-delete-product.component.css'
+})
+export class EditDeleteProductComponent {
+
+}
